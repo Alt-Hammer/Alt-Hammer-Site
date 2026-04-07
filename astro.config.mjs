ab74a72212c3021ca8502b6bc5ec74f084cb865a
Alt-Hammer <alex.cw.metcalfe@gmail.com>
2026-04-06 22:54:03 -0400

    Updates!?

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
 import keystatic from '@keystatic/astro';
 import netlify from '@astrojs/netlify';
 
 export default defineConfig({
   site: 'https://alt-hammer.netlify.app',
   output: 'server',
-  integrations: [keystatic()],
+  integrations: [react(), keystatic()],
   adapter: netlify(),
 });
